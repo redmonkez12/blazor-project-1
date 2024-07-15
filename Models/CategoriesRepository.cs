0503f17ee9b363c510a984a3019765434651b575
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-07-15 10:32:15 +0200

    Fix

--- Models/CategoriesRepository.cs
+++ Models/CategoriesRepository.cs
@@ -3,10 +3,10 @@ namespace Crypto.Models;
 public class CategoriesRepository
 {
     private static List<Category> _categories = [
-        new Category { Name = "Bitcoin Ecosystem"},
-        new Category { Name = "Ethereum Ecosystem"},
-        new Category { Name = "Polkadot Ecosystem"},
-        new Category { Name = "Cosmos Ecosystem"},
+        new Category { CategoryId = 1, Name = "Bitcoin Ecosystem"},
+        new Category { CategoryId = 2, Name = "Ethereum Ecosystem"},
+        new Category { CategoryId = 3, Name = "Polkadot Ecosystem"},
+        new Category { CategoryId = 4, Name = "Cosmos Ecosystem"},
     ];
     
     public static List<Category> GetCategories() => _categories;
